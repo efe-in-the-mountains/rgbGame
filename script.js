156d57d2150ad63d0e2cb1e232e104dd886a20ed
phillip shields <efe-in-the-mountains@users.noreply.github.com>
2017-11-15 14:01:36 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    // set initial colors for squares
+    squares[i].style.backgroundColor = colors[i]
+
+    // add click listener to squares
+    squares[i].addEventListener("click", function() {
+        // grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        // compare colors
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+
+// num should be equal to the num of squares - 6
+function generateRandomColors(num) {
+    // make array
+    var arr = [];
+    // add num random colors to array
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor())
+    }
+    // return finished array
+    return arr;
+}
+
+function randomColor () {
+    // pick r 0-255
+    var r = Math.floor(Math.random() * 256);
+    // pick g 0-255
+    var g = Math.floor(Math.random() * 256);
+    // pick b 0-255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+
+function changeColors(color) {
+    // loop through squares
+    for (let i = 0; i < squares.length; i++) {
+        // change each color to match pickedColor
+        squares[i].style.backgroundColor = color;
+    }
+}
